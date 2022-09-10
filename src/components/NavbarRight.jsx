08449b2601f66b1786c04c0d40d895b5d4e1b3af
siska199 <siska.apriana.1703226@students.um.ac.id>
2022-09-10 14:35:25 +0700

    add ui search message

--- src/components/NavbarRight.jsx
+++ src/components/NavbarRight.jsx
@@ -1,21 +1,37 @@
-import React from "react";
-import { BsArrowRight } from "react-icons/bs";
+import React, { useState } from "react";
+import { BsArrowRight, BsSearch, BsThreeDotsVertical } from "react-icons/bs";
 import ContactInfo from "./ContactInfo";
-import { ContainerNavbarRight } from "./navbarRight.css";
+import { ContainerNavbarRight, IconsContainer, Icon } from "./navbarRight.css";
 
 const NavbarRight = (props) => {
   const { setContactActive } = props;
+  const [searchActive, setSearchActive] = useState(false);
   const image =
     "https://i.pinimg.com/736x/94/50/00/945000e8ff2183b081d7c3e94add39d3.jpg";
   return (
-    <ContainerNavbarRight onClick={() => setContactActive(null)}>
+    <ContainerNavbarRight>
       <ContactInfo
         image={image}
         username={"User Test"}
         info={"online"}
         type={"profile-receiver-info"}
       />
-      <BsArrowRight className="icon" />
+      <IconsContainer>
+        {searchActive && <input />}
+        <Icon className="icon-search" active={searchActive} onClick={() => setSearchActive(searchActive ? false : true)}>
+          <BsSearch />
+        </Icon>
+        {!searchActive && (
+          <>
+            <Icon className="icon-setting">
+              <BsThreeDotsVertical />
+            </Icon>
+            <Icon className="icon-arrow-back">
+              <BsArrowRight onClick={() => setContactActive(null)} />
+            </Icon>
+          </>
+        )}
+      </IconsContainer>
     </ContainerNavbarRight>
   );
 };
--- src/components/navbarRight.css.js
+++ src/components/navbarRight.css.js
@@ -7,14 +7,47 @@ export const ContainerNavbarRight = styled.nav`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  .icon {
-    font-size: 2rem;
+  gap: 1rem;
+`;
+
+export const IconsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  font-size: 1.2rem;
+  color: white;
+  cursor: pointer;
+  flex-grow: 1;
+  justify-content: flex-end;
+  input {
+    width: 90%;
+    background-color: transparent;
+    border: none;
+    border-bottom: 2px solid #f3aecb;
+    outline: none;
     color: white;
-    margin-right: 0.6rem;
-    cursor: pointer;
+    padding: 0.3rem 0rem;
+  }
+`;
+
+export const Icon = styled.div`
+  width: 2.5rem;
+  height: 2.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  &:hover {
+    background-color: #8183be;
+  }
+  &.icon-search {
+    background-color: ${({ active }) => active && "#8183be"};
+  }
+  &.icon-arrow-back {
+    font-size: 1.5rem;
     display: none;
     @media (max-width: 767px) {
-      display: block;
+      display: flex;
     }
   }
 `;
